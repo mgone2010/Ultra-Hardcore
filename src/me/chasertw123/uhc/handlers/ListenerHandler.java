cf7749d1595f543ab42d44fe91566732aa78b736
Chase Henderson <henderson.chase@gmail.com>
2014-08-05 18:47:07 -0400

    Removed BlockPlace

--- src/me/chasertw123/uhc/handlers/ListenerHandler.java
+++ src/me/chasertw123/uhc/handlers/ListenerHandler.java
@@ -1,7 +1,6 @@
 package me.chasertw123.uhc.handlers;
 
 import me.chasertw123.uhc.Main;
-import me.chasertw123.uhc.listeners.BlockPlace;
 import me.chasertw123.uhc.listeners.ChunkPopulate;
 import me.chasertw123.uhc.listeners.EntityDeath;
 import me.chasertw123.uhc.listeners.EntityRegainHealth;
@@ -25,7 +24,7 @@ public class ListenerHandler {
 		PluginManager pm = plugin.getServer().getPluginManager();
 
 		plugin.getServer().getMessenger().registerOutgoingPluginChannel(plugin, "BungeeCord");
-		pm.registerEvents(new BlockPlace(plugin), plugin);
+		
 		pm.registerEvents(new ChunkPopulate(plugin), plugin);
 		pm.registerEvents(new EntityDeath(), plugin);
 		pm.registerEvents(new EntityRegainHealth(), plugin);
