14b787703b73f53c2789cf4e9a060d9ce5ca0c14
Tibo442 <Tibo.lebeggersca@gmail.com>
2014-08-14 19:56:34 +0200

    Fix PrepareItemCraft & send a nice message

--- src/me/chasertw123/uhc/listeners/PrepareItemCraft.java
+++ src/me/chasertw123/uhc/listeners/PrepareItemCraft.java
@@ -1,6 +1,9 @@
 package me.chasertw123.uhc.listeners;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
@@ -8,21 +11,27 @@ import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.inventory.ItemStack;
 
 public class PrepareItemCraft implements Listener {
-	
+
 	@EventHandler
 	public void onPrepareItemCraft(PrepareItemCraftEvent e) {
-		
+
 		CraftingInventory ci = e.getInventory();
-		
-		if (ci.getResult() == new ItemStack(Material.GOLDEN_CARROT))	
+
+		if (ci.getResult().getType() == Material.GOLDEN_CARROT)	
 			if (!ci.contains(new ItemStack(Material.GOLD_INGOT))) {
 				ci.setResult(null);
+				for (HumanEntity he : e.getViewers())
+					if (he instanceof Player)
+						((Player) he).sendMessage(ChatColor.RED + "You need 8 golden ingots instead of nuggets to craft this!");
 				return;
 			}
-		
-		else if (ci.getResult() == new ItemStack(Material.SPECKLED_MELON))
+
+		if (ci.getResult().getType() == Material.SPECKLED_MELON)
 			if (!ci.contains(new ItemStack(Material.GOLD_BLOCK))) {
 				ci.setResult(null);
+				for (HumanEntity he : e.getViewers())
+					if (he instanceof Player)
+						((Player) he).sendMessage(ChatColor.RED + "You need 1 golden block instead of the nuggets to craft this!");
 				return;
 			}
 	}
